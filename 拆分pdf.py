9d5a0105a8a1d143b212a320013bfd0d46e4932a
YANG Guanglu <thenkerlulu@gmail.com>
2017-10-09 12:44:58 +0800

    pdf

---
+++ 拆分pdf.py
@@ -0,0 +1,28 @@
+import os,io
+from PyPDF2 import PdfFileReader,PdfFileWriter,PdfFileMerger
+
+src =r"H:\清代硃卷集成PDF"
+dst =r"C:\Users\ygl\Desktop\扫描"
+
+lis = os.listdir(src)
+
+
+for f in lis:
+    name = f.split(".")[0]
+    dstpath = os.path.join(dst,name)
+    if not os.path.exists(dstpath):
+        os.mkdir(dstpath)
+    pdfbook = PdfFileReader(open(os.path.join(src,f),'rb'))
+    pnum =pdfbook.getNumPages()
+    for i in range(1,pnum):
+        pdfw =PdfFileWriter()
+        page =pdfbook.getPage(i)
+        pname = os.path.join(dstpath,"{:0>4d}".format(i)+".pdf")
+        pdfw.addPage(page)
+        pdfw.write(open(pname,'wb'))
+        
+
+    print(dstpath)
+
+print("结束")
+        
